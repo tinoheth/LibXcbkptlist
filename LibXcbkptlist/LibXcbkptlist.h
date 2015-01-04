3dfe8cd774894575788c3f5973d6f7501c0ce467
Tino Heth <th@t-no.de>
2015-01-04 16:48:50 +0100

    Initial Commit

---
+++ LibXcbkptlist/LibXcbkptlist.h
@@ -0,0 +1,19 @@
+//
+//  LibXcbkptlist.h
+//  LibXcbkptlist
+//
+//  Created by Tino Heth on 04.01.15.
+//  Copyright (c) 2015 Tino Heth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibXcbkptlist.
+FOUNDATION_EXPORT double LibXcbkptlistVersionNumber;
+
+//! Project version string for LibXcbkptlist.
+FOUNDATION_EXPORT const unsigned char LibXcbkptlistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibXcbkptlist/PublicHeader.h>
+
+
