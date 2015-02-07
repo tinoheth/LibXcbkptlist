9fd87023de98cc2f556877ca7768f554c8d55dc4
Tino Heth <th@t-no.de>
2015-02-07 15:14:26 +0100

    Bot commit

---
+++ XcbkptlistLib/XcbkptlistLib.h
@@ -0,0 +1,13 @@
+//
+//  XcbkptlistLib.h
+//  XcbkptlistLib
+//
+//  Created by Tino Heth on 07.02.15.
+//  Copyright (c) 2015 Tino Heth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XcbkptlistLib : NSObject
+
+@end
---
+++ XcbkptlistLib/XcbkptlistLib.m
@@ -0,0 +1,13 @@
+//
+//  XcbkptlistLib.m
+//  XcbkptlistLib
+//
+//  Created by Tino Heth on 07.02.15.
+//  Copyright (c) 2015 Tino Heth. All rights reserved.
+//
+
+#import "XcbkptlistLib.h"
+
+@implementation XcbkptlistLib
+
+@end
